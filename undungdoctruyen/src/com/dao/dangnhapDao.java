4ab17f90bcac2b309d55d05cc66c8699f5093aa4
temmaste <thinhho00191@gmail.com>
2022-11-26 01:07:57 +0700

    chức năng đăng kí

--- undungdoctruyen/src/com/dao/dangnhapDao.java
+++ undungdoctruyen/src/com/dao/dangnhapDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.model.Nguoidung;
 import com.model.Tacgia;
@@ -13,10 +14,20 @@ public class dangnhapDao {
 	 
 	public List<Nguoidung> getAll(){
 		Session ses = SessionUtil.createFac().openSession();
-		//Query query=ses.createQuery("From Chuong c where c.tap.matap=2");
 		Query query=ses.createQuery("From Nguoidung");
 		List<Nguoidung> dsnguoidung= query.list();
 		ses.close();
 		return dsnguoidung;
+	}
+	public void SignUp(String name,String username,String password) {
+		Session ses = SessionUtil.createFac().openSession();
+		ses.beginTransaction();
+		Nguoidung nd = new Nguoidung();
+		nd.setTentaikhoan(username);
+		nd.setMatkhau(password);
+		nd.setTennguoidung(name);
+		ses.save(nd);
+		ses.getTransaction().commit();
+		ses.close();
 	}
 }
--- undungdoctruyen/src/com/service/nguoidungService.java
+++ undungdoctruyen/src/com/service/nguoidungService.java
@@ -3,6 +3,7 @@ package com.service;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -26,4 +27,21 @@ public class nguoidungService {
 		}
 		return false;
 	}
+	
+	@POST
+	@Path("/dangki")
+	@Produces({ MediaType.APPLICATION_JSON})
+	public Boolean addAccount(@QueryParam("name")String name,@QueryParam("username")String username,@QueryParam("password")String password){
+		dangnhapDao dnd = new dangnhapDao();
+		dnd.SignUp(name, username, password);
+		//check username password 
+		List<Nguoidung> nd = new dangnhapDao().getAll();
+		for(Nguoidung usr :nd) {
+			if(usr.getTentaikhoan().equals(username)&&usr.getMatkhau().equals(password)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
