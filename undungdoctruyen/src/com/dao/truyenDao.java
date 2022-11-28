9fd68bd854cd6166416576ca0d81975346d38107
temmaste <thinhho00191@gmail.com>
2022-11-29 01:06:06 +0700

    b1

--- undungdoctruyen/src/com/dao/truyenDao.java
+++ undungdoctruyen/src/com/dao/truyenDao.java
@@ -3,9 +3,12 @@ package com.dao;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
-
+import com.model.Binhluan;
+import com.model.Nguoidung;
 import com.model.Truyen;
 import com.util.SessionUtil;
+
+import java.util.Date;
 import java.util.List;
 
 
@@ -18,5 +21,35 @@ public class truyenDao {
 		List<Truyen> dsTruyen= query.list();
 		ses.close();
 		return dsTruyen;
+	}
+	
+	public Truyen getIdTruyen(int id) {
+		Session ses = SessionUtil.createFac().openSession();
+		ses.beginTransaction();
+		Truyen tr =(Truyen) ses.get(Truyen.class, id);
+		ses.close();
+		return tr;
+	}
+	public Nguoidung getIdNguoidung(int id) {
+		Session ses = SessionUtil.createFac().openSession();
+		ses.beginTransaction();
+		Nguoidung tr =(Nguoidung) ses.get(Nguoidung.class, id);
+		ses.close();
+		return tr;
+	}
+	
+	public void addComment(String message,int manguoidung,int matruyen,Date ngaydang) {
+		
+		Session ses = SessionUtil.createFac().openSession();
+		ses.beginTransaction();
+		Binhluan bl = new Binhluan();
+		bl.setMessage(message);
+		bl.setNgaydang(ngaydang);
+		bl.setTruyen(getIdTruyen(matruyen));
+		bl.setNguoidung(getIdNguoidung(manguoidung));
+		ses.save(bl);
+		ses.getTransaction().commit();
+		ses.close();
+		
 	}
 }
--- undungdoctruyen/src/com/service/test.java
+++ undungdoctruyen/src/com/service/test.java
@@ -1,10 +1,22 @@
 package com.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import com.dao.truyenDao;
+
 public class test {
 
 	public static void main(String[] args) {
  
-
+		truyenDao t = new truyenDao();
+		try {
+			 
+			t.addComment("daqua pessi oi", 3, 2, new SimpleDateFormat("yyyy-MM-dd").parse("2011-01-18"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
